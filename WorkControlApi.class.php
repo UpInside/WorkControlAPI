473577da047b98e16dc0d89e36efb416a916500d
Robson V. Leite <cursos@upinside.com.br>
2016-06-21 12:39:37 -0300

    Update to Order By

--- WorkControlApi.class.php
+++ WorkControlApi.class.php
@@ -11,6 +11,7 @@ class WorkControlApi {
     private $Key;
     private $Token;
     private $cUrl;
+    private $Error;
 
     /**
      * Inicia a conexão com a API nativa do Work Control para obter qualquer tipo de dado!
@@ -26,21 +27,31 @@ class WorkControlApi {
 
     /**
      * Obtém posts de um site com Work Control. Você pode configurar o limte, salto, categoria, e ordem da lista a obter!
-     * @param type $Limit = Quantidade de posts
-     * @param type $offset = Saltar posts
-     * @param type $CategoryId = Id da categoria dos posts
-     * @param type $Order = Ordenação <b>1</b> para ASC, <b>2</b> para DESC e <b>3</b> para RAND
-     * @return type Array com todos os dados para looping nativo!
+     * @param INT $Limit = Quantidade de posts!
+     * @param INT $offset = Saltar posts!
+     * @param INT $CategoryId = Id da categoria dos posts!
+     * @param INT $Order = Ordenação <b>1</b> para ASC, <b>2</b> para DESC e <b>3</b> para RAND!
+     * @param INT $By = Campo de ordem <b>1 ou null</p> pela data, ou <b>2</b> pela quantidade de visitas!
+     * @return INT Array com todos os dados para looping nativo!
      */
-    public function getPosts($Limit = null, $offset = null, $CategoryId = null, $Order = null) {
-        $this->cUrl = $this->Base . "/_api/post.php?key={$this->Key}&token={$this->Token}&limit={$Limit}&offser={$offset}&catid={$CategoryId}&order={$Order}";
+    public function getPosts($Limit = null, $offset = null, $CategoryId = null, $Order = null, $By = null) {
+        $this->cUrl = $this->Base . "/_api/post.php?key={$this->Key}&token={$this->Token}&limit={$Limit}&offser={$offset}&catid={$CategoryId}&order={$Order}&by={$By}";
 
         $curl = curl_init($this->cUrl);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($curl);
         curl_close($curl);
 
-        return json_decode($result, true);
+        if (empty(json_decode($result)->error)):
+            return json_decode($result, true);
+        else:
+            $this->Error = json_decode($result)->error;
+            return false;
+        endif;
+    }
+
+    public function getError() {
+        return $this->Error;
     }
 
 }
